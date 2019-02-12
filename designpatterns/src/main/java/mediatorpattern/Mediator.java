5c80a7a8068ef65e68793d5388f0621b83b6e075
JustJavaIt <sky87865742@icloud.com>
2019-02-12 18:04:07 +0800

    中介模式 add

---
+++ designpatterns/src/main/java/mediatorpattern/Mediator.java
@@ -0,0 +1,6 @@
+package mediatorpattern;
+
+public abstract class Mediator {
+    //申明一个联络方法
+    public abstract void constact(String message,Person person);
+}
---
+++ designpatterns/src/main/java/mediatorpattern/Person.java
@@ -0,0 +1,11 @@
+package mediatorpattern;
+
+public class Person {
+    protected String name;
+    protected Mediator mediator;
+
+    Person(String name,Mediator mediator){
+        this.name = name;
+        this.mediator = mediator;
+    }
+}
